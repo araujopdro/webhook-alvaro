dfdbf91321c6384732fd183158de3d1be550a3eb
Pedro Araujo <pedro.detroit57@hotmail.com>
2024-12-04 13:16:43 -0300

    body response

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.post('/webhook', (req, res) => {
     const data = req.body;
     console.log(data); // Processar os dados recebidos
-    res.status(200).send({ status: 'success' });
+    res.status(200).send({ status: 'success', body: {...req.body} });
 });
 
 app.listen(PORT, () => {
