61dab565ddc0618d52a4528b0517c07542867178
Pedro Araujo <pedro.detroit57@hotmail.com>
2024-12-20 16:54:15 -0300

    un console.log

--- index.js
+++ index.js
@@ -92,8 +92,13 @@ app.get('/posicaoCondutor', (req, res) => {
 function HandleMachineStatus(e){
     const event_corrida_idx = corridas_to_process.findIndex((c) => c.id_corrida === e.id_mch)
     const event_corrida = event_corrida_idx >= 0 ? corridas_to_process[event_corrida_idx] : null;
-    if(event_corrida == null) return
-    console.log('\x1b[46m%s\x1b[0m', `${event_corrida && bot_headers[event_corrida.bot_id] ? bot_headers[event_corrida.bot_id].bot_name+' | ' : ''}Corrida: ${e.id_mch} (${e.status_solicitacao})`)
+    console.log(e.m)
+    if(event_corrida == null) {
+        console.log('\x1b[41m%s\x1b[0m', `Corrida: ${e.id_mch} (${e.status_solicitacao})`)
+        return
+    } else {
+        console.log('\x1b[46m%s\x1b[0m', `${event_corrida && bot_headers[event_corrida.bot_id] ? bot_headers[event_corrida.bot_id].bot_name+' | ' : ''}Corrida: ${e.id_mch} (${e.status_solicitacao})`)
+    }
     let fluxo_name
     switch(e.status_solicitacao){
         case 'L':
