e2fa0d46a71bc91ebb45b3361ba8840b8b76a148
Pedro Araujo <pedro.detroit57@hotmail.com>
2025-02-12 22:28:50 -0300

    pooling push log motorista chegou

--- index.js
+++ index.js
@@ -128,9 +128,9 @@ async function ProcessCorridas() {
                 if (is_in_range) {
 
                     const origin = bot_headers[corrida.bot_id.replace(/\s/g, "")].bot_name
-                    console.log('\x1b[43m%s\x1b[0m', `${corrida.id_corrida} - ${origin} | (${response.data.response.status}): Corrida finalizada/cancelada. | ${cur_date}`)
-                    corridas_to_process[corrida.id_corrida].logs.push(`${corrida.id_corrida} - ${origin} | (${response.data.response.status}): Corrida finalizada/cancelada. | ${cur_date}`)
-                    
+                    console.log('\x1b[43m%s\x1b[0m', `${corrida.id_corrida} - ${origin} | (X): Motorista chegou. | ${cur_date}`)
+                    corridas_to_process[corrida.id_corrida].logs.push(`${corrida.id_corrida} - ${origin} | (X): Motorista chegou. | ${cur_date}`)
+
                     SendPulseFlowToken(corrida.bot_id, corrida.contact_id, 'notifica-motorista-chegou', corrida.id_corrida)
                     corridas_to_process[corrida.id_corrida].get_position = false;
                 }
